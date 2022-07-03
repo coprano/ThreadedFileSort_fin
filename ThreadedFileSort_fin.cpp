bac9d94adc49e72db4a5cbfb64a4a38e985a1b45
coprano <73130317+coprano@users.noreply.github.com>
2022-07-03 15:39:49 +0300

    merge_two_files needs fixing. everything else works.

--- ThreadedFileSort_fin.cpp
+++ ThreadedFileSort_fin.cpp
@@ -13,29 +13,27 @@ const int BigFileSize = 1;            //размер большого файла
 //////////////////////////////////////////////////////////////////////////
 
 
-//const int BiGFileCount = int(ceil(BigFileSize * 1024 * 1024 / sizeof(int32_t)));
-//const int SmallFileCount = int(ceil(SmallFileSize * 1024 * 1024 / sizeof(int32_t)));
 
 
 /*создает файл, заполняет его, закрывает файл*/
-int create_big_file(const char* FileName) {
+int create_big_file(const char* FName) {
     random_device dev;
     mt19937 rng(dev());
     uniform_int_distribution<mt19937::result_type> dist(0, maxN);
 
     FILE* f;
-    errno_t err = fopen_s(&f, FileName, "wb+");
+    errno_t err = fopen_s(&f, FName, "wb+");
 
-    if (f == NULL)
+    if (f == NULL) {
+        cout << "create_big_file: err opening file. code:" << err << endl;
         return -1;
-    //int BiGFileCount = int(ceil(BigFileSize * 1024 * 1024 / sizeof(int32_t)));
-    int BiGFileCount = int(ceil(BigFileSize * 1024 * 1024 / sizeof(int32_t)));
+    }
+    int BiGFileCount = int(ceil(BigFileSize * 1024 / sizeof(int32_t)));
     for (int i = 0; i < BiGFileCount; i++)
     {
         int32_t a = dist(rng);
         fwrite(&a, sizeof(int32_t), 1, f);
     };
-    cout << "BFC: " << BiGFileCount << endl;
     fclose(f);
     return 0;
 }
@@ -43,7 +41,7 @@ int create_big_file(const char* FileName) {
 /*выводит на экран все числа из файла размера BigFile*/
 int show_file(const char* fname) {
     FILE* f;
-    errno_t err = fopen_s(&f, FileName, "rb+");
+    errno_t err = fopen_s(&f, fname, "rb+");
 
     if (f == NULL)
         return -1;
@@ -63,12 +61,13 @@ int sort(const char* fname)
     // нужно открыть файл и прочитать количество элементов
     FILE* f;
     errno_t err = fopen_s(&f, fname, "rb+");
-    if (f == NULL)
+    if (f == NULL) {
+        cout << "sort: err opening file. code:" << err << endl;
         return -1;
+    }
 
     int n;
     n = _filelength(_fileno(f)) / sizeof(int32_t);
-    cout << endl << " N: " << n << endl << endl;
     int32_t* tmp = new int32_t[1];
     // пока не равно количеству елементов
     for (int i = 0; i <= n; i++)
@@ -115,13 +114,17 @@ int merge_two_files(const char* f1name, const char* f2name, const char* fresname
     errno_t errfres = fopen_s(&fres, fresname, "wb+");
 
 
-    if (f1 == NULL or f2 == NULL or fres == NULL)
+    if (f1 == NULL or f2 == NULL or fres == NULL) {
+        cout << "create_big_file: err opening file. codes:" << errf1 << ":" << errf2 << ":" << errfres << endl;
         return -1;
+    }
     bool fin = false;
     int n1, n2;
-    while (fread(&n1, sizeof(int32_t), 1, f1) != NULL or fread(&n2, sizeof(int32_t), 1, f2) != NULL) {
+    while (!fin) {
+        auto s1 = fread(&n1, sizeof(int32_t), 1, f1);
+        auto s2 = fread(&n2, sizeof(int32_t), 1, f2);
         //если в обоих файлах есть числа
-        if (fread(&n1, sizeof(int32_t), 1, f1) != NULL and fread(&n2, sizeof(int32_t), 1, f2) != NULL) {
+        if (s1 != NULL and s2!=NULL) {
             if (n1 > n2) {
                 fwrite(&n2, sizeof(int32_t), 1, fres);
                 fseek(f2, sizeof(int32_t), SEEK_CUR);
@@ -139,23 +142,23 @@ int merge_two_files(const char* f1name, const char* f2name, const char* fresname
             }
             else cout << "err in merge_two_files idk why" << endl;
         }
-        else if (fread(&n1, sizeof(int32_t), 1, f1) == NULL and fread(&n2, sizeof(int32_t), 1, f2) != NULL) {
+        else if (s1 == NULL and s2 != NULL) {
             while (fread(&n2, sizeof(int32_t), 1, f2) != NULL) {
                 fwrite(&n2, sizeof(int32_t), 1, fres);
                 fseek(f2, sizeof(int32_t), SEEK_CUR);
-                fflush(f1);
+                fflush(f2);
                 cout << "case2 n1:n2 " << n1 << " " << n2 << endl;
             }
         }
-        else if (fread(&n2, sizeof(int32_t), 1, f2) == NULL and fread(&n1, sizeof(int32_t), 1, f1) != NULL) {
+        else if (s1 != NULL and s2 == NULL) {
             while (fread(&n1, sizeof(int32_t), 1, f1) != NULL) {
                 fwrite(&n1, sizeof(int32_t), 1, fres);
                 fseek(f1, sizeof(int32_t), SEEK_CUR);
-                fflush(f2);
+                fflush(f1);
                 cout << "case3 n1:n2 " << n1 << " " << n2 << endl;
             }
         }
-        if (fread(&n2, sizeof(int32_t), 1, f2) == NULL and fread(&n1, sizeof(int32_t), 1, f1) == NULL) {
+        if (s1 == NULL and s2 == NULL) {
             fin = true;
             cout << "fin" << endl;
         }
@@ -170,18 +173,21 @@ int merge_two_files(const char* f1name, const char* f2name, const char* fresname
 
 int main()
 {
-    cout << (false or false) << endl;
-    const char* FileName1 = "File1MB";
-    const char* FileName2 = "File2MB";
-    const char* FileNameRes = "FileRes";
     setlocale(LC_ALL, "RUS");
+    const char* FileName1 = "File1";
+    const char* FileName2 = "File2";
+    const char* FileNameRes = "FileRes";
+
     create_big_file(FileName1);
-    //create_big_file(FileName2);
+    create_big_file(FileName2);
     sort(FileName1);
     sort(FileName2);
+
     merge_two_files(FileName1, FileName2, FileNameRes);
+    show_file(FileName2);
+    cout << endl << endl;
     show_file(FileNameRes);
 
-    system("pause");
+
     return 0;
 }
